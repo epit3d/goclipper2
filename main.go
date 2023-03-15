1153113df83bca755304e785cdcaddf6f26cd1ad
Lev Kozlov <kozlov.l.a@mail.ru>
2023-03-15 16:51:21 +0300

    chore: add sample main

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/epit3d/goclipper2/goclipper2"
 )
 
 func main() {
-	p := goclipper2.NewPaths64()
+	p := goclipper2.NewPath64()
 
-	p.Simplify()
+	p.AddPoint(*goclipper2.NewPoint64(0, 0))
+
+	log.Println(p)
 }
